fa7ca8984b01ad510661d179c971544437f1529c
Saurav Sadanandan <sauravsadanandan@gmail.com>
2026-02-21 22:18:32 +0530

    edited search test file

--- tests/test_poshmark_search.py
+++ tests/test_poshmark_search.py
@@ -8,7 +8,7 @@ def test_poshmark_search(page : Page, base_url: str):
 	search_page.item_search("Nike shoes")
 	search_page.apply_filter("availability", "Available Items")
 
-	expect(search_page.search_title).to_have_text("Search results for : ")
+	expect(search_page.search_title).to_contain_text("Search results for : ")
 
 	expect(search_page.return_filter("availability")).to_be_visible()
 	expect(search_page.sold_tag).to_have_count(0)
