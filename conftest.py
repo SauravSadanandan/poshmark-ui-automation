1a079d3f43f9106f220a866e66bae5d48eaaca5c
Saurav <sauravsadanandan@gmail.com>
2026-03-19 01:13:38 +0530

    added stealth mode in confest

--- conftest.py
+++ conftest.py
@@ -35,7 +35,15 @@ def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
         "storage_state": "other_scripts/posh_login.json",
-        "viewport": {"width": 1920, "height": 1080}
+        "viewport": {"width": 1920, "height": 1080},
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    }
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
+    return {
+        **browser_type_launch_args,
+        "args": ["--disable-blink-features=AutomationControlled"]
     }
 
 @pytest.fixture(scope="session")
